cab838884d5a3c55746dc987aae6bd1f5f60c1f4
laem99 <c9laem00@students.oamk.fi>
2021-11-30 09:02:35 +0200

    10 a.2

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('This is message :D');
   return (
     <View style={styles.container}>
       <Text>This is test</Text>
